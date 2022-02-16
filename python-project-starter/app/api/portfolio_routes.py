b17c522f2834058757ebac96d325dcb2935d0277
Jake <jweber04005@msn.com>
2022-02-16 12:34:05 -0500

    Working Buy Additional/Sell Partial/Sell All

--- python-project-starter/app/api/portfolio_routes.py
+++ python-project-starter/app/api/portfolio_routes.py
@@ -14,6 +14,7 @@ def get_user_portfolio(userId):
         stock = Stock.query.get(s.id)
         res[s.id] = {
             "id": s.id,
+            "stockId": s.stockId,
             "stockName": stock.name,
             "currentPrice": dumps(stock.price),
             "ticker": stock.ticker,
@@ -24,19 +25,41 @@ def get_user_portfolio(userId):
 
 
 
-@portfolio_routes.route('/add/<int:stockId>/<int:userId>')
+@portfolio_routes.route('/add/<int:stockId>/<int:userId>', methods=["PUT"])
 def add_to_portfolio(stockId, userId):
     portfolio = Portfolio.query.filter(userId == Portfolio.userId).filter(stockId == Portfolio.stockId).first()
     user = User.query.get(userId)
     stock = Stock.query.get(stockId)
     data = request.get_json()
 
+
     if (stock.price * data['amount'] > user.balance):
         return 'Not Enough Money.'
 
-    portfolio.purchasePrice = ((portfolio.purchasePrice * portfolio.amount) + (stock.price * data['amount'])) / (portfolio.amount + data['amount'])
-    portfolio.amount = portfolio.amount + data['amount']
-    user.balance = stock.price * data['amount']
+    portfolio.purchasePrice = ((portfolio.purchasePrice * portfolio.count) + (stock.price * data['amount'])) / (portfolio.count + data['amount'])
+    user.balance = user.balance - stock.price * (data['amount'] - portfolio.count)
+    portfolio.count = data['amount']
     db.session.commit()
 
     return portfolio.to_dict()
+
+@portfolio_routes.route('/sell/<int:stockId>/<int:userId>', methods=["PUT", "DELETE"])
+def sell_stock(stockId, userId):
+    portfolio = Portfolio.query.filter(userId == Portfolio.userId).filter(stockId == Portfolio.stockId).first()
+    user = User.query.get(userId)
+    stock = Stock.query.get(stockId)
+
+    data = request.get_json()
+
+
+    if request.method == 'DELETE':
+        user.balance = user.balance + (stock.price * portfolio.count)
+        db.session.delete(portfolio)
+        db.session.commit()
+        return "Stock sold"
+
+    if request.method == 'PUT':
+        user.balance = user.balance + (stock.price * (portfolio.count - data['amount']))
+        portfolio.count = portfolio.count - (portfolio.count - data['amount'])
+        db.session.commit()
+        return portfolio.to_dict()
